38717bdc5d4160b35fc24f5ac8531e9986f334d4
helmiar527 <helmiar527@gmail.com>
2022-06-17 11:26:33 +0700

    upload 4

--- bootstrap/dist/js/custom.js
+++ bootstrap/dist/js/custom.js
@@ -1,20 +1,12 @@
 const page1 = document.getElementById('page1');
 const page2 = document.getElementById('page2');
 const more = document.getElementById('more');
-// const body = document.getElementsById('particles-js');
-const content = document.getElementById('particles-js');
-const a = document.querySelector('div#particles-js canvas'); 
+const body = document.querySelector('body');
 
-// if (partikel.classList.contains('particles-js-canvas-el')) {
-//     content.innerHTML = '<h1>Hello World</h1>';
-//     console.log('input');
-// }
-
-
-// if (more.addEventListener("click", e => {
-//   page1.classList.add('d-none');
-//   body.classList.remove('bg-purple');
-//   page2.classList.remove('d-none');
-//   body.classList.add('bg-purple-rgba');
-//   console.log("click");
-// }));
+if (more.addEventListener("click", e => {
+  body.classList.remove('bg-black');
+  body.classList.add('bg-purple');
+  page1.classList.add('d-none');
+  page2.classList.remove('d-none');
+  console.log("click");
+}));
